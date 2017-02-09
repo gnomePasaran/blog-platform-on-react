dffe07e19d817dc004b8c636deabe72426b6a580
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-02-09 23:12:00 +0300

    add prepareData callback

--- src/routes/Blog.js
+++ src/routes/Blog.js
@@ -1,18 +1,27 @@
 import MainLayoutContainer from 'components/layouts/MainLayoutContainer';
 import About               from 'components/views/About';
-import BlogPageContainer   from 'components/containers/BlogPage/BlogPageContainer';
-import PostContainer       from 'components/containers/Post/PostContainer';
+import PostsContainer      from 'containers/PostsContainer';
+import PostContainer       from 'containers/PostContainer';
 
 import { aboutPath, postsPath } from 'helpers/routes';
 
+import { fetchPosts } from 'actions/Posts';
+import { fetchPost } from 'actions/Post';
+
 const Index = {
   path: '/',
-  component: BlogPageContainer
+  component: PostsContainer,
+  prepareData: (store) => {
+    store.dispatch(fetchPosts());
+  }
 };
 
 const PostRoutes = {
   path: postsPath(),
-  component: PostContainer
+  component: PostContainer,
+  prepareData: (store, query, params) => {
+    store.dispatch(fetchPost(params.id));
+  }
 };
 
 const AboutRoutes = {
