16eb9587d695af553d8d025ffae7fc7aa27af298
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-04-04 23:47:54 +0300

    add expanded meta-tags

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -1,15 +1,23 @@
 import React, { PropTypes } from 'react';
 
+import Helmet from 'react-helmet';
+
 import { Segment, Header, Container } from 'semantic-ui-react';
 import Link from 'components/elements/Link';
 
 const MainLayout = (props) => (
+  <div>
+    <Helmet>
+      <meta name="description" content="React Blog Platform" />
+      <link rel="canonical" href="http://my-react-site.com" />
+    </Helmet>
   <Container>
     <Logo />
     { props.goBackButton }
     { props.children }
     <Footer />
   </Container>
+  </div>
 );
 
 MainLayout.propTypes = {
