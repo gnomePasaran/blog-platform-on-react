c397f0e2f7734348e623c5e5f861de4e8945bd06
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-05-14 22:13:07 +0300

    change PropTypes

--- src/components/views/elements/Form/DateField.js
+++ src/components/views/elements/Form/DateField.js
@@ -1,4 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import DatePicker from 'components/views/elements/Form/DatePicker';
 import formatDate from 'helpers/formatDate';
 import RenderField from 'components/views/elements/Form/RenderFields';
--- src/components/views/widgets/BlogItem.js
+++ src/components/views/widgets/BlogItem.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Image from 'components/views/elements/Image';
 import TextBox from 'components/views/elements/TextBox';
